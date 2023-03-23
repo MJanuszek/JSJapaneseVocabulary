3e3f98c48e97c82623404044d8cce44b64d7b4a4
MJanuszek <monika.januszek.mail@gmail.com>
2023-03-23 13:10:28 +0100

    select word, compare with input.value

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
 const words = [
-    {id: 1, word: "momo", meaningPL: "brzoskwinia", meaningANG: "peach"},
-    {id: 2, word: "akai", meaningPL: "czerwony", meaningANG: "red"},
-    {id: 3, word: "aoi", meaningPL: "niebieski", meaningANG: "blue"},
-    {id: 4, word: "midori", meaningPL: "zielony", meaningANG: "green"},
-    {id: 5, word: "enpitsu", meaningPL: "ołówek", meaningANG: "pencil"},
+    {id: 1, word: "momo", meaningPL: "brzoskwinia", meaningANG: "peach", category: "noun"},
+    {id: 2, word: "akai", meaningPL: "czerwony", meaningANG: "red", category: "adjective"},
+    {id: 3, word: "aoi", meaningPL: "niebieski", meaningANG: "blue", category: "adjective"},
+    {id: 4, word: "midori", meaningPL: "zielony", meaningANG: "green", category: "adjective"},
+    {id: 5, word: "enpitsu", meaningPL: "ołówek", meaningANG: "pencil", category: "noun"},
+    {id: 6, word: "iku", meaningPL: "iść", meaningANG: "to go", category: "verb"},
 ]
 
 const showBtn = document.querySelector(".show-btn");
 const checkBtn = document.querySelector(".checkCorrect");
 const wordDisplay = document.querySelector(".word-display");
-
+const category = document.getElementById("category");
+let valueToCompare = document.getElementById("valueToCompare");
+// ---------------------
+let currentInputValue = '';
+let currentWord = '';
+let filteredWords;
+let filtered;
 
 
 const showJapaneseWord = () => {
-  
-    let currentWord = words[Math.floor(Math.random()*words.length)];
+    
+    filteredWords = words.filter((item) => (item.category === category.value));
+    // filtered = words.filter((item) => item.category === "rzeczownik");
+    
+    // 
+    currentWord = filteredWords[Math.floor(Math.random()*filteredWords.length)];
     console.log(currentWord.word);
     wordDisplay.textContent = currentWord.word
 }
 
+// get value form input to compare if correct:
+valueToCompare.addEventListener("change", (e)=> {
+    currentInputValue = e.target.value;
+    
+})
+// compare input.value with drawn word:
+const checkIfCorrectAnswer = () => {
+    if(currentInputValue.toLowerCase() === currentWord.word.toLowerCase()){
+        console.log("poprawna odpowiedź");
+    } else {
+        console.log("niepoprawna odpowiedź");
+    }
+    valueToCompare.value = "";
+}
+
 showBtn.addEventListener("click", showJapaneseWord);
+checkBtn.addEventListener("click", checkIfCorrectAnswer);
